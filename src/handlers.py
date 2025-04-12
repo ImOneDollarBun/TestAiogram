587e08d724042fba3ec7a982de12d8a7451fb29f
potatomaster <potatomaster>
2025-04-12 20:58:56 +0900

    upgrade url copy

--- src/handlers.py
+++ src/handlers.py
@@ -41,7 +41,7 @@ async def input_file(message: Message, bot: Bot):
                 break
         s = ''
         for x in report.values():
-            s += ''.join(x[0]) + '\n' + ''.join(f'`{x[1]}`') + '\n' + ''.join(x[2])
+            s += ''.join(x[0]) + '\n' + ''.join(f'`{x[1]}`') + '\n' + ''.join(x[2]) + '\n'
             s += '\n'
 
         await message.answer(s, parse_mode='Markdown')
